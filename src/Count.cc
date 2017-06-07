c2ebed2cb2d676241a2dd4f06d725f4f0647b6f8
Brad Solomon <bsolomo1@arctic.compbio.cs.cmu.edu>
2017-06-07 18:51:58 -0400

    Restored the code block in 'bt count' which is responsible for writing the output filter...

--- src/Count.cc
+++ src/Count.cc
@@ -131,12 +131,12 @@ bool split_count(
     const auto jf_ary = mer_hash.ary();
     const auto end = jf_ary->end();
     std::cerr << "Right before cutoff count: " << cutoff_count << std::endl;
-/*    for(auto kmer = jf_ary->begin(); kmer != end; ++kmer) {
+    for(auto kmer = jf_ary->begin(); kmer != end; ++kmer) {
         auto& key_val = *kmer;
         if (key_val.second >= cutoff_count) {
             bf.add(key_val.first);
         }
-    }*/
+    }
     bf.save();
     return true;
 }
